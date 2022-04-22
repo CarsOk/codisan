58df3a1d2423a33f6cfef37ee9d8b2e66d0d202d
Migueljimenz <migueljiarteta@gmail.com>
2022-04-21 21:42:11 -0500

    se implemento gema device en el proyecto

--- Proyecto_Codisan/app/controllers/application_controller.rb
+++ Proyecto_Codisan/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:type_document, :document, :first_name, :second_name, :first_last_name, :second_last_name, :email, :phone])
+  end
 end
