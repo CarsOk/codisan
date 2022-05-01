00377fc3ee079458fd075726be79982f1ea7536d
Migueljimenz <migueljiarteta@gmail.com>
2022-05-01 10:02:20 -0500

    correccion
    se corrigieron los bugs al momento del inicio de sesion y la creacion de usuarios

--- Proyecto_Codisan/db/schema.rb
+++ Proyecto_Codisan/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_204427) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_30_224513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_204427) do
     t.datetime "updated_at", null: false
     t.string "second_name"
     t.string "second_last_name"
-    t.integer "document"
+    t.string "document"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
