f705bdf5daf18f7b5e6b65eb9a9534dc11c80ff0
KevinCastro01 <kevinmcf04@gmail.com>
2022-05-13 14:44:33 -0500

    Alertas del desarrollo.
    Se cambió el idioma de las alertas del ingles al español.

--- Proyecto_Codisan/app/helpers/application_helper.rb
+++ Proyecto_Codisan/app/helpers/application_helper.rb
@@ -13,13 +13,11 @@ module ApplicationHelper
     end
     
     def flash_class(key)
-
         case key
         when 'notice' then 'alert alert-info'
         when 'success' then 'alert alert-success'
         when 'error' then 'alert alert-danger'
         when 'alert' then 'alert alert-warning'
-        end
-        
+        end 
     end
 end
