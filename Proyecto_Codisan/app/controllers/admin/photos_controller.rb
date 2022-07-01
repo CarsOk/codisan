356636e6afe0cf7f7b8a001e55294be2b4dffa17
Migueljimenz <migueljiarteta@gmail.com>
2022-07-01 16:09:07 -0500

    model-photo-information-add-admin
    se agregaron los modelos de fotos y de informacion dentro de la carpeta admin

---
+++ Proyecto_Codisan/app/controllers/admin/photos_controller.rb
@@ -0,0 +1,60 @@
+class Admin::PhotosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_photo, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      flash[:success] = "foto creada correctamente"
+      redirect_to admin_photos_path
+    else
+      flash[:alert] = "Error al guardar la foto"
+      render :new
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    if @photo.update(photo_params)
+      flash[:success] = "foto actualizada correctamente"
+      redirect_to admin_photos_path
+    else
+      flash[:alert] = "Error al actualizar la foto"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @photo.destroy
+      flash[:success] = "foto eliminada"  
+      redirect_to admin_photos_path
+    else
+      flash[:alert] = "Error al eliminar la foto"
+    end
+  end
+
+  private
+
+   def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:name_photo, :description, :avatar)
+  end
+end
